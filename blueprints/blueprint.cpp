ad02d433386ce53a93e15dd1496f223ffff58656
Niko Köser <koeserniko@gmail.com>
2019-03-04 02:13:22 +0100

    Add blueprints folder

---
+++ blueprints/blueprint.cpp
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Title:
+ * Project:
+ * Author:
+ * Description:
+ ******************************************************************************/
+
+/* Includes */
+#include <iostream>
+
+/* Declarations */
+
+/* Main function */
+int main() {
+    std::cout << "Hello world!\n";
+    return 0;
+}
+
+/* Definitions */
