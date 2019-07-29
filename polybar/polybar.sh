87910fe9013aed65920a447374f1276eca0abc93
Niko Köser <koeserniko@gmail.com>
2019-07-29 02:29:03 +0200

    Update launch script

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 3; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-#  Launch polybar
-polybar default &
+# Launch bar
+polybar top &
+
+echo "Bar launched..."
