e8272228d0a5583cebe895130c1a393961c54a12
Niko Köser <koeserniko@gmail.com>
2019-08-28 13:02:44 +0200

    Remove comment
    Remove useless comment.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,6 +3,9 @@
 # Terminate already running bar instances
 killall -q polybar
 
+# Wait for wal
+# while [ ! $(pgrep -x wal) ]; do; done
+
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
