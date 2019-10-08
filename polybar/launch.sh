05a80b24eba5322c6ce33e0118b7dee48a6a130f
Niko Köser <koeserniko@gmail.com>
2019-10-08 12:31:27 +0200

    Update monitor detection loop
    The loop should print the found monitors.
    
    This change adds an echo statement that prints the monitors that were
    found. Useful for debugging purposes.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,19 +3,18 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait for wal
-# while [ ! $(pgrep -x wal) ]; do; done
-
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload top &
-  done
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload top &
+        echo "Bar launched on monitor ${m}..."
+    done
 else
-  polybar --reload top &
+    polybar --reload top &
+    echo "Bar launched..."
 fi
 
-echo "Bar launched..."
+
